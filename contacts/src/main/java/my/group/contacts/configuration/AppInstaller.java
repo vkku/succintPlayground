955eea8c853ad0fd4ddeec03624fdbf2b0422e27
vkku <kumar.vivekten@gmail.com>
2019-09-06 08:15:38 +0530

    Protype from Archetype

---
+++ contacts/src/main/java/my/group/contacts/configuration/AppInstaller.java
@@ -0,0 +1,10 @@
+package my.group.contacts.configuration;
+
+import com.venky.swf.configuration.Installer;
+public class AppInstaller implements Installer{
+
+  public void install() {
+    
+  }
+}
+
